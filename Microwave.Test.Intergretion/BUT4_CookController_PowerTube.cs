0c17a84043e2c2ab45588b8e5334e302b4cc87f3
Eva-Vib-And <60734490+Eva-Vib-And@users.noreply.github.com>
2021-04-28 13:39:12 +0200

    Update BUT4_CookController_PowerTube.cs

--- Microwave.Test.Intergretion/BUT4_CookController_PowerTube.cs
+++ Microwave.Test.Intergretion/BUT4_CookController_PowerTube.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
 using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Intergretion
@@ -10,12 +12,23 @@ namespace Microwave.Test.Intergretion
     class BUT4_CookController_PowerTube
     {
         private IPowerTube powerTube;
+        private IDisplay display;
+        private ILight light;
+        private IUserInterface UI;
+        private ITimer timer;
         private CookController cookController;
+        private IOutput output;
 
         [SetUp]
         public void SetUp()
         {
-            powerTube.
+            output = Substitute.For<IOutput>();
+            powerTube = new PowerTube(output);
+            display = new Display(output);
+            light = new Light(output);
+            timer = Substitute.For<ITimer>();
+            UI = Substitute.For<IUserInterface>();
+           cookController = new CookController(timer, display, powerTube, UI);
         }
 
     }
