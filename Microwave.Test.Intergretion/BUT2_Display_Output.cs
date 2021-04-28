5137dbc7cc0ad5f51236cd8444a0999012f3fdb7
Ayaan A. Ali <ayaan_ali13@hotmail.com>
2021-04-28 09:35:59 +0200

    .

---
+++ Microwave.Test.Intergretion/BUT2_Display_Output.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microwave.Test.Intergretion
+{
+    public class BUT2_Display_Output
+    {
+
+        [Setup]
+        public void Setup()
+        {
+
+        }
+    }
+}
