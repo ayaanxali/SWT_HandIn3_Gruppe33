ca700c8a4a007bd0857e6151012efeed8661a8d0
Eva-Vib-And <60734490+Eva-Vib-And@users.noreply.github.com>
2021-04-28 13:13:25 +0200

    Begyndende SetUp for testklassen CookControl

--- Microwave.Test.Intergretion/BUT4_CookController_PowerTube.cs
+++ Microwave.Test.Intergretion/BUT4_CookController_PowerTube.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
 using NUnit.Framework;
 
 namespace Microwave.Test.Intergretion
 {
     class BUT4_CookController_PowerTube
     {
+        private IPowerTube powerTube;
+        private CookController cookController;
+
         [SetUp]
         public void SetUp()
         {
-
+            powerTube.
         }
 
     }
